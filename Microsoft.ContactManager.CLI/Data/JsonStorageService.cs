2d32a10daa015b0219560ff1e986cdc62106026f
marim55555 <lym969620@gmail.com>
2026-02-23 22:06:49 +0200

    Add ContactService with in-memory storage and auto-increment ID

---
+++ Microsoft.ContactManager.CLI/Data/JsonStorageService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ContactManager.CLI.Data
+{
+    internal class JsonStorageService
+    {
+    }
+}
---
+++ Microsoft.ContactManager.CLI/Services/ContactService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ContactManager.CLI.Services
+{
+    internal class ContactService
+    {
+    }
+}
